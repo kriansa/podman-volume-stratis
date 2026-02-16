1fa2fdbdbac8beb2f7bb0f39f8150747b6f9c44d
Daniel Pereira <daniel@garajau.com.br>
2026-02-16 15:36:13 -0300

    fix: remove stratisd override on package uninstall

--- build/packaging/scripts/preremove.sh
+++ build/packaging/scripts/preremove.sh
@@ -6,4 +6,7 @@ systemctl disable podman-volume-stratis 2>/dev/null || true
 systemctl stop stratisd-fix-ordering.path 2>/dev/null || true
 systemctl disable stratisd-fix-ordering.path 2>/dev/null || true
 
+# Remove the stratisd.service override we created to fix the ordering cycle
+rm -f /etc/systemd/system/stratisd.service
+
 systemctl daemon-reload
